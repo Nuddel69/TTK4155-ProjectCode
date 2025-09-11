2cf6c9e90d034d509d89575609fb86f37433afd5
Svein-Thore <wighus92@gmail.com>
2025-09-11 15:57:13 +0200

    Prøver igjen

--- drivers/uart.c
+++ drivers/uart.c
@@ -7,7 +7,10 @@
 char receive_buf = 0;
 ISR(USART0_RXC_vect) { USART_Receive(); }
 
+<<<<<<< Updated upstream
 void USART_init(struct USART_config *config) {
+=======
+>>>>>>> Stashed changes
   uint16_t ubrr = (config->fosc / 16 / config->baud) - 1;
   UBRR0H = (unsigned char)(ubrr >> 8);
   UBRR0L = (unsigned char)ubrr;
@@ -69,3 +72,12 @@ void USART_ReceiveHandler() {
     cmd_count = 0;
   }
 }
+
+// Disable global interrupts
+cli( );
+
+EMCUCR |= (1<<ISC2 ) ;
+GICR |= (1<<INT2 ) ;
+
+sei( );
+

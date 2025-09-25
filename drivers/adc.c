b5c62231ab3f2dc11a2043d5c827175fce5f668b
Mats Bjønnes <mats.bjonnes@gmail.com>
2025-09-25 15:39:03 +0200

    ADC: Incomplete adc driver code

--- drivers/adc.c
+++ drivers/adc.c
@@ -1,5 +1,38 @@
+#include <avr/delay.h>
+#include <avr/io.h>
 #include <errno.h>
 
 #include "adc.h"
 
-int adc_init() { return 0; }
+#define ADC_BASE_ADR ((volatile uint8_t *)0x1000) // Adress offset for ADC
+
+int adc_init(void) {
+  // Set read and start conversion pins for ADC
+
+  DDRB |= (1 << PB1) | (1 << PB2);
+
+  return 0;
+}
+
+// Read all channels sequentially after BUSY goes high (3.6 us per ch)
+void ADC_read(uint8_t *read_to_adr) {
+  for (uint8_t i = 0; i < 3; i++) {
+    // Select channel (sets PA1 and PA2 depending on channel, before pulsing RD)
+    DDRA = 0xFF; // Set to outputs
+    PINA = i;
+
+    // Send WR
+    PINB ^= (1 << PB1); // Toggle WR
+    DDRA = 0x00;        // Set to inputs
+    // Safe delay, 5 us per channel (with 5 MHz CLK) should be enough.
+    _delay_us(5);
+
+    PINB ^= (1 << PB2); // ToggleRD
+    _delay_us(5);
+    // Read channel on the ADC
+    volatile uint8_t data = PINA;
+    read_to_adr[i] = data;
+  }
+}
+
+int adc_ready(void) { return 0; }

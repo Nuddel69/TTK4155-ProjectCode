e2ed2631e073cc6b02cf3c1991f0b75622ed6ffd
Mats Bjønnes <mats.bjonnes@gmail.com>
2025-09-25 09:14:46 +0200

    IO: Added driver-shell for OLED and joystick

--- drivers/io.c
+++ drivers/io.c
@@ -3,9 +3,89 @@
 #include "adc.h"
 #include "io.h"
 
-int io_oled_init(struct oled_device *dev, int *status);
+#define JOYSTICK_THRESHOLD 10
 
-int io_joystick_read(struct joystick_device *dev, int *buffer) {
+//------------------//
+//     Joystick     //
+//------------------//
+
+int io_joystick_init(struct io_joystick_device *dev) {
+  int status = 0;
+
+  if (adc_ready()) {
+    status = adc_init();
+  }
+
+  status = io_joystick_calibrate(dev);
+
+  return status;
+}
+int io_joystick_read_position(struct io_joystick_device *dev,
+                              struct io_joystick_position *buffer) {
   return -ENOERR;
 }
-int io_joystick_calibrate(struct joystick_device *dev) { return -ENOERR; }
+int io_joystick_read_direction(struct io_joystick_device *dev,
+                               enum io_joystick_direction *direction) {
+  int status = 0;
+
+  struct io_joystick_position pos;
+  status = io_joystick_read_position(dev, &pos);
+  if (status) {
+    return status;
+  }
+
+  if (pos.x > (0 - JOYSTICK_THRESHOLD) && pos.x < (0 + JOYSTICK_THRESHOLD) &&
+      pos.y > (0 - JOYSTICK_THRESHOLD) && pos.y < (0 + JOYSTICK_THRESHOLD)) {
+    *direction = NEUTRAL;
+    return status;
+  }
+
+  // Fill inn the remaining directions...
+
+  return status;
+}
+int io_joystick_calibrate(struct io_joystick_device *dev) {
+  int status = 0;
+
+  struct io_joystick_position pos;
+  status = io_joystick_read_position(dev, &pos);
+  if (status) {
+    return status;
+  }
+
+  dev->x_offset = pos.x * -1;
+  dev->y_offset = pos.y * -1;
+
+  return -ENOERR;
+}
+
+//------------------//
+//       OLED       //
+//------------------//
+
+int io_oled_init(struct io_oled_device *dev) {
+  int status = 0;
+
+  while (spi_ready(&dev->spi)) {
+    status = spi_init(&dev->spi);
+  }
+
+  /*/
+   *
+   * OLED STUFF
+   *
+  /*/
+
+  return status;
+}
+int io_oled_reset(struct io_oled_device *dev) { return -ENOERR; }
+int io_oled_home(struct io_oled_device *dev) { return -ENOERR; }
+int io_oled_goto_line(struct io_oled_device *dev, int line) { return -ENOERR; }
+int io_oled_goto_column(struct io_oled_device *dev, int column) {
+  return -ENOERR;
+}
+int io_oled_clear_line(struct io_oled_device *dev, int line) { return -ENOERR; }
+int io_oled_pos(struct io_oled_device *dev, int row, int column) {
+  return -ENOERR;
+}
+int io_oled_print(struct io_oled_device *dev, char *text) { return -ENOERR; }
--- include/io.h
+++ include/io.h
@@ -3,19 +3,68 @@
 
 #include "spi.h"
 
-struct oled_device {
+struct io_oled_device {
   struct spi_interface spi;
   unsigned int cs;
 };
-
-struct joystick_device {
+struct io_joystick_device {
   unsigned int adc_address; // Dunno what's needed, placeholder field
+  int x_offset;
+  int y_offset;
 };
+struct io_joystick_position {
+  int x; // X value in % from the middle
+  int y; // Y value in % from the middle
+};
+enum io_joystick_direction { LEFT, RIGHT, UP, DOWN, NEUTRAL };
 
-int io_oled_init(struct oled_device *dev, int *status);
+//------------------//
+//     Joystick     //
+//------------------//
 
-int io_joystick_init(struct joystick_device *dev);
-int io_joystick_read(struct joystick_device *dev, int *buffer);
-int io_joystick_calibrate(struct joystick_device *dev);
+/*!
+ * \brief Initialise and calibrate a joystick.
+ * \param[in] dev The joystick to be initlaised.
+ * \return Errno.
+ */
+int io_joystick_init(struct io_joystick_device *dev);
 
+/*!
+ * \brief Read the position of a joystick as a percentage from 0 in each axis.
+ * \param[in] dev The joystick to read.
+ * \param[in] buffer A buffer for storing the position value.
+ * \return Errno.
+ */
+int io_joystick_read_position(struct io_joystick_device *dev,
+                              struct io_joystick_position *buffer);
+
+/*!
+ * \brief Fetches the direction of a joystick.
+ * \param[in] dev The joystick to read.
+ * \param[in] direction A buffer for storing the read direction.
+ * \return Errno.
+ */
+int io_joystick_read_direction(struct io_joystick_device *dev,
+                               enum io_joystick_direction *direction);
+
+/*!
+ * \brief Calibrate a joystick. Requires the joystick to be in its neutral
+ * position.
+ * \param[in] dev The joystick to calibrate.
+ * \return Errno.
+ */
+int io_joystick_calibrate(struct io_joystick_device *dev);
+
+//------------------//
+//       OLED       //
+//------------------//
+
+int io_oled_init(struct io_oled_device *dev);
+int io_oled_reset(struct io_oled_device *dev);
+int io_oled_home(struct io_oled_device *dev);
+int io_oled_goto_line(struct io_oled_device *dev, int line);
+int io_oled_goto_column(struct io_oled_device *dev, int column);
+int io_oled_clear_line(struct io_oled_device *dev, int line);
+int io_oled_pos(struct io_oled_device *dev, int row, int column);
+int io_oled_print(struct io_oled_device *dev, char *text);
 #endif // INCLUDE_INCLUDE_IO_H_

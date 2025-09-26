1907ae421992df024fc2e78648440e31447c2fd3
Svein-Thore <wighus92@gmail.com>
2025-09-26 12:13:42 +0200

    ADC works on XMEM pheriferal interface

--- drivers/adc.c
+++ drivers/adc.c
@@ -5,34 +5,27 @@
 
 #define ADC_BASE_ADR   ((volatile uint8_t*)0x1000) //Adress offset for ADC
 
+
 void ADC_init(void) {
 
-    //Set read and start conversion pins for ADC as outputs
-    DDRB |= (1<<PB1)|(1<<PB2);
+    // XMEM controls the ADC and is enabled elsewhere (xmem_init())
+    // Do NOT touch DDR/PORTA (PORTA is the external data bus and is handeled trough writing to and reading to xmem)
+    // No need to set up PD6/PD7; XMEM controls WR/RD automatically when we read or write to memory allocted to the ADC
+
 }
 
 void ADC_start_conv(void){
 
-    PORTB &= ~(1 << PB1);//Toggle WR LOW
-    //Delay to ensure ADC conversion is done, consider checking BUSY pin insead.
+    // Write to any adress on ADC to trigger WR
+    *ADC_BASE_ADR = 0x00;
+    // Delay to ensure ADC conversion is done, consider checking BUSY pin insead.
     _delay_us(5);
-    PORTB |= (1 << PB1);//Toggle WR HIGH
 }
 
 uint8_t ADC_read_ch(void){
 
-    //Send WR
-    PORTB &= ~(1 << PB2);//Toggle RD LOW
-    //Delay, 1 us (with 1 MHz CLK) should be enough
-    _delay_us(1);
-
-    //Read datapins of the ADC
-    volatile uint8_t data = PINA;
-
-    PORTB |= (1 << PB2);//Toggle RD HIGH
-    _delay_us(1);
-
-    return data;
+    // Read from any adress in ADC allocated memory adressarea to 
+    return *ADC_BASE_ADR;
 }
 
 
@@ -45,6 +38,7 @@ void ADC_read_all(ADC_meas *output) {
     //Send a pulse then read for each channel
     for (uint8_t i = 0; i < 4; i++) {
         output->channel[i] = ADC_read_ch();
+        _delay_us(5);
     }
 }
 
@@ -56,9 +50,9 @@ void ADC_test(void){
     ADC_read_all(&data);
 
     printf("--ADC Test--\n\r");
-    printf("CH0=%3u  CH1=%3u  CH2=%3u  CH3=%3u\r\n", data.channel[0], data.channel[1], data.channel[2], data.channel[3]);
+    printf("CH0=%3d  CH1=%3d  CH2=%3d  CH3=%3d\r\n", data.channel[0]-128, data.channel[1]-128, data.channel[2]-128, data.channel[3]-128);
     
-    _delay_ms(3000);
+    _delay_ms(100);
 }
 
 

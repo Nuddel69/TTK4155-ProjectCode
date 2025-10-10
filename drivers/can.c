1acb8ca298d7828e38e864a1999fe3f9d2fdf632
Svein-Thore <wighus92@gmail.com>
2025-10-10 12:45:17 +0200

    CAN: forgot to save befor commit, on last

---
+++ drivers/can.c
@@ -0,0 +1,32 @@
+
+#include <stdint.h>
+
+int8_t CAN_init(){
+
+}
+
+int8_t CAN_init(){
+
+}
+
+int8_t CAN_read(){
+
+}
+
+int8_t CAN_write(){
+
+}
+
+int8_t CAN_request_to_send(){
+
+}
+
+int8_t CAN_bit_modify(){
+
+}
+int8_t CAN_reset(){
+
+}
+int8_t CAN_read_status(){
+
+}

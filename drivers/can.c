549c65ae410ae5481a9aba408e3377be881710d3
Svein-Thore <wighus92@gmail.com>
2025-10-10 13:10:01 +0200

    CAN: Added function from lab lecture

--- drivers/can.c
+++ drivers/can.c
@@ -1,16 +1,33 @@
 
+#include <avr/io.h>
 #include <stdint.h>
+#include "spi.h"
 
-int8_t CAN_init(){
 
+int8_t CAN_init(struct *dev){
+
+uint8_t value ;
+SPI_init(); // Initialize SPI
+CAN_reset(); // Send reset - command
+// Self - test
+CAN_read(MCP_CANSTAT,&value);
+if ((value&MODE_MASK)!=MODE_CONFIG){
+    printf("MCP2515 is NOT in configuration mode after reset !\n");
+    return 1;
+    }
+return 0;
 }
 
-int8_t CAN_init(){
+//Taken directly from lab lecture
+int8_t CAN_read(struct *dev,uint8_t address){
 
-}
-
-int8_t CAN_read(){
-
+    uint8_t result;
+    PORTB &= ~(1<<CAN_CS);                 // Select CAN - controller  -- Not sure if this is correct pin for us
+    spi_send(dev,MCP_READ);                // Send read instruction
+    spi_send(dev,address);                 // Send address
+    pi_recieve(spi_slave,adress);          // Read result
+    PORTB |= (1<<CAN_CS);                  // Deselect CAN - controller -- Not sure if this is correct pin for us
+    return 0;
 }
 
 int8_t CAN_write(){

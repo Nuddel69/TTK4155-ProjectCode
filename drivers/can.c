9e423f8f3788e9fb8b91048c3346c9bf20732fe7
Svein-Thore <wighus92@gmail.com>
2025-10-10 17:07:06 +0200

    CAN: Added more MCP2515 specific functions

--- drivers/can.c
+++ drivers/can.c
@@ -1,49 +1,121 @@
-
 #include <avr/io.h>
 #include <stdint.h>
 #include "spi.h"
 
+//------------------//
+//   GENERAL CAN    //
+//------------------//
+
 
 int8_t CAN_init(struct *dev){
 
-uint8_t value ;
-SPI_init(); // Initialize SPI
-CAN_reset(); // Send reset - command
-// Self - test
-CAN_read(MCP_CANSTAT,&value);
-if ((value&MODE_MASK)!=MODE_CONFIG){
-    printf("MCP2515 is NOT in configuration mode after reset !\n");
-    return 1;
-    }
 return 0;
 }
 
-//Taken directly from lab lecture
 int8_t CAN_read(struct *dev,uint8_t address){
 
     uint8_t result;
-    PORTB &= ~(1<<CAN_CS);                 // Select CAN - controller  -- Not sure if this is correct pin for us
-    spi_send(dev,MCP_READ);                // Send read instruction
-    spi_send(dev,address);                 // Send address
-    pi_recieve(spi_slave,adress);          // Read result
-    PORTB |= (1<<CAN_CS);                  // Deselect CAN - controller -- Not sure if this is correct pin for us
+    PORTB &= ~(1<<CAN_CS);             // Select CAN 
+    spi_send(dev,MCP_READ);            // Send read instruction
+    spi_send(dev,address);             // Send address
+    spi_recieve(spi_slave,adress);     // Read result
+    PORTB |= (1<<CAN_CS);              // Deselect CAN 
     return 0;
 }
 
-int8_t CAN_write(){
+//------------------//
+// MCP2515 Specific //
+//------------------//
 
+
+int8_t init_MSCP2515(){
+    uint8_t value ;
+
+    while (spi_ready()) { // Confirm SPI Initialized
+        status = spi_init();
+    } 
+    MCP2515_reset(); // Send reset - command
+
+    // Confirm that we are in correct mode after boot
+    MCP2515_read_status(dev,&value);
+    uint8_t mode= (0x7&value>>5);     // Extract Mode from status
+    uint8_t irq = (0x07&(&value>>1)); // Extract Interrupt requests from status
+    if (mode!= MODE_CONFIG){
+        printf("MCP2515 is NOT in configuration mode after reset !\n");
+        return 1;
+    }
+    if(irq != 0){
+        printf("There is an interrupt request when booting the CAN")
+        return 2;
+    }
+    //Set Config
+    MCP2515_bit_modify(dev,);
+    //Set RX Masks
+    MCP2515_bit_modify(dev,);
+
+    
+    //REST OF CAN INIT GOES HERE
+
+
+
+    return 0;
 }
 
-int8_t CAN_request_to_send(){
+// Write data to register beginning at selected address.
+int8_t MCP2515_write(*dev,uint8_t *data){
 
+    spi_send(dev,0b00000010);      //
+    spi_send(dev,);
+    MCP2515_request_to_send(dev,1); 
+
+    return 0;
 }
 
-int8_t CAN_bit_modify(){
+// Read a specefic RX BUFFER see command at pg 66 in CAN controller datasheet
+int8_t MCP2515_read(struct *dev,uint8_t rx_buf_num, uint8_t *out){
 
+    uint8_t cmd = (0b10010000|(rx_buf_num<<1))
+    spi_send(dev,cmd);
+    spi_recieve(dev, uint8_t out);
+
+    return 0;
 }
-int8_t CAN_reset(){
 
+// Instructs controller to begin message transmission sequence for
+// any of the transmit buffers. 
+int8_t MCP2515_request_to_send(struct *dev,uint8_t tx_buf_num){
+
+    uint_t cmd = (0b10000000|tx_buf_num);
+    spi_send(dev,cmd);
+
+    return 0;
 }
-int8_t CAN_read_status(){
 
+
+// Set or clear individual bits in a particular register
+int8_t MCP2515_bit_modify(struct *dev, uint8_t reg, uint8_t set_val){
+
+    spi_send(dev,0b00000101);
+    spi_send(dev,reg);
+    spi_send(dev,set_val);
+
+    return 0;
+}
+
+// Resets internal registers to default state, set Configuration mode
+int8_t MCP2515_reset(struct *dev){
+
+    spi_send(dev,0b11000000);
+
+    return 0;
+}
+
+// Quick polling command that reads several status bits for transmit and receive functions.
+int8_t MCP2515_read_status(struct *dev,uint8_t *out){
+
+    uint8_t *status;
+    spi_send(dev, 0b01010000);
+    spi_recieve(dev, uint8_t out);
+
+    return 0;
 }
--- include/can.h
+++ include/can.h
@@ -1,20 +1,75 @@
 #ifndef INCLUDE_CAN_H_
 #define INCLUDE_CAN_H_
 
+#include <stdint.h>
+#include <avr/io.h>
+
+
+#define MCP_CANSTAT
+#define MODE_MASK    
+#define CAN_CS       //Dont know whichone we are using yet
+#define MCP_READ     0b00000011
+#define CAN_STCONFIG 0b00000110
+
 struct can_msg{
     uint8_t adress;
     uint8_t num_df;
     uint8_t data;
 };
 
+//See page 59 datasheet on controller
+enum MODE{
+    MODE_NORMAL      = 0b000,
+    MODE_SLEEP       = 0b001,
+    MODE_LOOPBACK    = 0b010,
+    MODE_LISTEN_ONLY = 0b011,
+    MODE_CONFIG      = 0b100
+}
+
+enum CAN_IRQ{
+    000 = NO_IRQ    = 0b000,
+    001 = ERROR_IRQ = 0b001,
+    010 = WAKEUP_IRQ = 0b010,
+    011 = TXB0_IRQ  = 0b011,
+    100 = TXB1_IRQ  = 0b100,
+    101 = TXB2_IRQ  = 0b101,
+    110 = RXB0_IRQ  = 0b110,
+    111 = RXB1_IRQ  = 0b111
+}
+
+enum CAN_BUF_ADR{
+    TXB0SIDH = 0x31,
+    TXB0D0   = 0x36,
+    TXB1SIDH = 0x41,
+    TXB1D0   = 0x46,
+    TXB2SIDH = 0x51,
+    TXB2D0   = 0x56,
+    RXB0SIDH = 0x61,
+    RXB0D0   = 0x66,
+    RXB1SIDH = 0x71,
+    RXB1D0   = 0x76,
+}
+
+
+//------------------//
+//   GENERAL CAN    //
+//------------------//
+
 int8_t CAN_init(void);
 int8_t CAN_read(void);
-int8_t CAN_write(void);
-int8_t CAN_request_to_send(void);
-int8_t CAN_bit_modify(void);
 int8_t CAN_reset(void);
-int8_t CAN_read_status(void);
 
 
+//------------------//
+//   MSCP2515    //
+//------------------//
+
+int8_t init_MSCP2515(struct io_oled_device *dev);
+int8_t MSCP2515_read(struct *dev,uint8_t rx_buf_num, uint8_t *out);
+int8_t MSCP2515_write(*dev,uint8_t *data);
+int8_t MSCP2515_request_to_send(struct *dev,uint8_t tx_buf_num);
+int8_t MSCP2515_bit_modify(struct *dev, uint8_t reg, uint8_t set_val);
+int8_t MSCP2515_reset(struct io_oled_device *dev);
+int8_t MSCP2515_read_status(struct io_oled_device *dev);
 
 #endif // INCLUDE_CAN_H_

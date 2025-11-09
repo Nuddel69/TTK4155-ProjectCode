1a023f45fcc9670bdaf16adb40e421c8a082c79b
Mats Bjønnes <mats.bjonnes@gmail.com>
2025-11-09 14:33:13 +0100

    Node 2: Servo: Servo controllable by percentage

--- ATSAM_Code/drivers/servo.c
+++ ATSAM_Code/drivers/servo.c
@@ -4,34 +4,36 @@
 #include "sam.h"
 #include "servo.h"
 
-#define _DTY_MIN 0x30F6  // 0.9 ms
-#define _DTY_MID 0x2F6C  // 1.5 ms
-#define _DTY_MAX 0x2DE2  // 2.1 ms
-#define _DTY_STEP 0x2DE2 // 2.1 ms
+#define _DTY_MIN 0x30F6 // 0.9 ms
+#define _DTY_MID 0x2F6C // 1.5 ms
+#define _DTY_MAX 0x2DE2 // 2.1 ms
+#define _DTY_STEP
 
 uint8_t servo_init(struct Servo_device *dev) {
   PWM_init(&dev->pwm);
   return 0;
 }
 
-static uint8_t servo_max_dty(struct Servo_device *dev) {
+static uint8_t _servo_max_dty(struct Servo_device *dev) {
   PWM_set_dty(&dev->pwm, dev->max_dty);
   return 0;
 }
 
-static uint8_t servo_min_dty(struct Servo_device *dev) {
+static uint8_t _servo_min_dty(struct Servo_device *dev) {
   PWM_set_dty(&dev->pwm, dev->min_dty);
   return 0;
 }
 
 uint8_t servo_set_percentage(struct Servo_device *dev, uint8_t position) {
   if (position >= 100) {
-    servo_max_dty(dev);
+    _servo_max_dty(dev);
   }
   if (position <= 0) {
-    servo_min_dty(dev);
+    _servo_min_dty(dev);
   }
 
-  PWM_set_dty(&dev->pwm, position * ((dev->max_dty - dev->min_dty) / 100));
+  PWM_set_dty(&dev->pwm,
+              dev->min_dty +
+                  (position * ((dev->max_dty - dev->min_dty) / 100)));
   return 0;
 }

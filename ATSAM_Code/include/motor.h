0590a4986873d4ad13ad54bb60ed06d70aad8131
Mats Bjønnes <mats.bjonnes@gmail.com>
2025-11-10 12:25:20 +0100

    Node 2: Motor: Motor init

---
+++ ATSAM_Code/include/motor.h
@@ -0,0 +1,23 @@
+#ifndef INCLUDE_INCLUDE_MOTOR_H_
+#define INCLUDE_INCLUDE_MOTOR_H_
+
+#include "pwm.h"
+#include "sam.h"
+#include <stdint.h>
+
+struct motor_device {
+  Pio *enpw;
+  uint8_t enqw_pin;
+  Pio *phdi;
+  uint8_t phdi_pin;
+  float period;
+  struct PWM_device _enpw_dev;
+};
+
+uint8_t motor_init(struct motor_device *dev);
+
+uint8_t motor_left(struct motor_device *dev, uint8_t speed);
+uint8_t motor_right(struct motor_device *dev, uint8_t speed);
+uint8_t motor_stop(struct motor_device *dev);
+
+#endif // INCLUDE_INCLUDE_MOTOR_H_

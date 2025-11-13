6e02fba47eb2d0f0254a801eca41eda14cbdc114
Mats Bjønnes <mats.bjonnes@gmail.com>
2025-11-13 10:24:03 +0100

    Node 2: Misc: Fix more bugs

--- ATSAM_Code/main.c
+++ ATSAM_Code/main.c
@@ -10,9 +10,10 @@
 #include <stdio.h>
 // #include <util/delay.h>
 
+#include "IR.h"
 #include "can_controller.h"
+#include "can_interrupt.h"
 #include "encoder.h"
-#include "ir.h"
 #include "led.h"
 #include "motor.h"
 #include "pid.h"
@@ -28,78 +29,77 @@
 
 // TEMP message for testing sending to node 1
 CAN_MESSAGE dummy_msg = {0x8, 8, {"HiWorld"}};
-	
-	
-struct io_joystick_position joy_pos = {0,0};
-struct io_avr_buttons btn;
-	
-	
-int process_can_frame(){
-	
-	CAN_MESSAGE msg;
-	if (can_rxq_pull(&msg)){
-		
-		switch (msg.id){
-			
-			case CAN_ID_ERROR:{   //This ID is reserved for errors, BOTH node1 and node2
-				
-				// TODO: handle error / stop game
-				
-				break;
-			}
-			case CAN_ID_GAMEOVER:{//This ID is reserved for gameover message from node2
-				
-				
-				break;
-			}
-			case CAN_ID_GAMESTART:{//This ID is reserved for starting a new game from node1
-				
-				// TODO: start game
-				
-				break;
-			}
-			case CAN_ID_JOYPOS:{  //This ID is reserved for sending Joystick position and button state
-				
-				joy_pos.x = (int8_t)msg.data[0];
-				joy_pos.y = (int8_t)msg.data[1];
-				btn.right = (uint8_t)msg.data[2];
-				btn.left  = (uint8_t)msg.data[3];
-				btn.nav   = (uint8_t)msg.data[4];
 
-				//printf("%c[2J",27);
-				printf("Buttons R=0x%02X L=0x%02X N=0x%02X, pos x:%d, y:%d\r",
-				btn.right, btn.left, btn.nav, joy_pos.x, joy_pos.y);
-				// update_control(joy_pos, btn);
-				
-				//update_control(joy_pos, btn); //TODO
-				
-				
-				break;
-			}
-			case CAN_ID_SOLONOID:{//This ID is reserved for sending trigger signal for the solonoid
-				
-				// TODO: trigger solonoid
-				
-				break;
-			}
-			case CAN_ID_MOTORPOS:{//This ID is reserved for sending current motor position
-				
-				break;
-			}
-			case CAN_ID_SCORE:{   //This ID is reserved for sending gamescore
-				
-				break;
-			}
-			case CAN_ID_DEFAULT:{ //This ID is for anything else
-				
-				break;
-			}
-			default:{
-				
-			}
-		}
-	}
-	
+struct io_joystick_position joy_pos = {0, 0};
+struct io_avr_buttons btn;
+
+int process_can_frame() {
+
+  CAN_MESSAGE msg;
+  if (can_rxq_pull(&msg)) {
+
+    switch (msg.id) {
+
+    case CAN_ID_ERROR: { // This ID is reserved for errors, BOTH node1 and node2
+
+      // TODO: handle error / stop game
+
+      break;
+    }
+    case CAN_ID_GAMEOVER: { // This ID is reserved for gameover message from
+                            // node2
+
+      break;
+    }
+    case CAN_ID_GAMESTART: { // This ID is reserved for starting a new game from
+                             // node1
+
+      // TODO: start game
+
+      break;
+    }
+    case CAN_ID_JOYPOS: { // This ID is reserved for sending Joystick position
+                          // and button state
+
+      joy_pos.x = (int8_t)msg.data[0];
+      joy_pos.y = (int8_t)msg.data[1];
+      btn.right = (uint8_t)msg.data[2];
+      btn.left = (uint8_t)msg.data[3];
+      btn.nav = (uint8_t)msg.data[4];
+
+      // printf("%c[2J",27);
+      printf("Buttons R=0x%02X L=0x%02X N=0x%02X, pos x:%d, y:%d\r", btn.right,
+             btn.left, btn.nav, joy_pos.x, joy_pos.y);
+      // update_control(joy_pos, btn);
+
+      // update_control(joy_pos, btn); //TODO
+
+      break;
+    }
+    case CAN_ID_SOLONOID: { // This ID is reserved for sending trigger signal
+                            // for the solonoid
+
+      // TODO: trigger solonoid
+
+      break;
+    }
+    case CAN_ID_MOTORPOS: { // This ID is reserved for sending current motor
+                            // position
+
+      break;
+    }
+    case CAN_ID_SCORE: { // This ID is reserved for sending gamescore
+
+      break;
+    }
+    case CAN_ID_DEFAULT: { // This ID is for anything else
+
+      break;
+    }
+    default: {
+    }
+    }
+  }
 }
 
 // struct PWM_device servo_pwm = ;
@@ -150,10 +150,9 @@ int main(void) {
   uint32_t counter = 0;
 
   while (1) {
-	  
-	  process_can_frame();
-	  pwm_dir_and_speed(&motor, &motor_pid, joy_pos.x*10);
-	  
+
+    process_can_frame();
+    pwm_dir_and_speed(&motor, &motor_pid, joy_pos.x * 10);
   }
   return 0;
 }
